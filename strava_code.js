19ff69c183ce3c63a976a3bd71a96e7b1127bd9f
Sam Nowak <68253285+SamNowak@users.noreply.github.com>
2024-04-24 09:30:50 -0500

    Update strava_code.js
    Call for new refresh token making sure that we have a non-expired one.

--- strava_code.js
+++ strava_code.js
@@ -1,6 +1,8 @@
+//https://www.strava.com/oauth/token?client_id=&client_secret=&refresh_token=305c2ace0e6e1c5830cfee115f47200b837773e3&grant_type=refresh_token
+const super_link = "https://www.strava.com/oauth/token"
+
 //making a fetch request for all activities
 //https://www.strava.com/api/v3/athlete/activities?access_token=277bc08d2d53600fbd22a05598fa1d479e3f7f24
-
 function getAllActivites() {
     const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=277bc08d2d53600fbd22a05598fa1d479e3f7f24"
     fetch(activities_link)
@@ -8,3 +10,25 @@ function getAllActivites() {
 }
 
 getAllActivites();
+
+//getting new access token
+function reAuthorize(){
+    fetch(super_link,{
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+
+            client_id: '122561',
+            client_secret: 'ffeb9025442e924abee07eb17cc98b976e76b7b4',
+            refresh_token: '305c2ace0e6e1c5830cfee115f47200b837773e3',
+            grant_type: 'refresh_token',
+
+        }).then((res) => console.log(res.json()))
+    })
+}
+
+reAuthorize();
