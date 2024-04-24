1b5456fdffbf61f598152b749ac81ca74101cccb
Sam Nowak <68253285+SamNowak@users.noreply.github.com>
2024-04-24 18:59:48 -0500

    Update strava_code.js

--- strava_code.js
+++ strava_code.js
@@ -4,7 +4,6 @@ const super_link = "https://www.strava.com/oauth/token"
 //making a fetch request for all activities
 //https://www.strava.com/api/v3/athlete/activities?access_token=
 function getAllActivites(res) {
-    res.access_token = undefined;
     const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
     fetch(activities_link)
         .then((res) => console.log(res.json()))
@@ -25,7 +24,7 @@ function reAuthorize(){
             client_id: '122561',
             client_secret: 'ffeb9025442e924abee07eb17cc98b976e76b7b4',
             refresh_token: '305c2ace0e6e1c5830cfee115f47200b837773e3',
-            grant_type: 'refresh_token',
+            grant_type: 'refresh_token'
 
         })
     }).then(res => res.json())
